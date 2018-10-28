907edcf99f66a1d0bfeb9b7b87b0fd09c4fe7b75
Ashley King <aking4645@gmail.com>
2018-10-28 15:33:32 -0400

    Adding standard navigation bar

---
+++ myScripts.js
@@ -0,0 +1,8 @@
+//openNav() and closeNav() allow for the side navigation bar to open and close
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
