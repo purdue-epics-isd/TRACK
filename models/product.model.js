6a1108952a5367d05a083ba01a0d9503ff0ec9f6
jessicajiang <jessicajiang5@gmail.com>
2018-10-29 21:16:35 -0400

    adding mongoose and track.js

---
+++ models/product.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let ProductSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+    price: {type: Number, required: true},
+});
+
+
+// Export the model
+module.exports = mongoose.model('Product', ProductSchema);
---
+++ track.js
@@ -0,0 +1,5 @@
+// track.js
+const express = require('express');
+const bodyParser = require('body-parser');
+// initialize our express app
+const app = express();
