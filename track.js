151695c0714518c28a21401396a95dd6eb185416
jessicajiang <jessicajiang5@gmail.com>
2018-11-18 22:24:14 -0500

    test controller working

--- track.js
+++ track.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
+app.use('/products', product);
+
 let port = 1234;
 
 app.listen(port, () => {
