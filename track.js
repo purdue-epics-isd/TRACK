252442bf4968e62c40039041108b4af83ba939bb
jessicajiang <jessicajiang5@gmail.com>
2018-11-18 23:03:52 -0500

    password in here but that's ok

--- track.js
+++ track.js
@@ -5,6 +5,17 @@ const bodyParser = require('body-parser');
 const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
+//Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://TRACK:woofwoofTRACKER7@ds255403.mlab.com:55403/track';
+let mongoDB = process.env.MONGO_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
 let port = 1234;
@@ -12,3 +23,4 @@ let port = 1234;
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
 });
+
