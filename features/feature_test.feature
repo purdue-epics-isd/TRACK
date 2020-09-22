b6c066ff7af360ed7d2d0b1baa1a697194bb1b98
anakeeb <anakeeb@purdue.edu>
2020-09-21 20:48:58 -0400

    worked on getting testing tutorial done

---
+++ features/feature_test.feature
@@ -0,0 +1,5 @@
+Feature: Testing tutorial
+	Scenario: Finding some cheese
+	   Given I am on the Google search page
+	   When I search for "Cheese!"
+	   Then the page title should start with "cheese"
---
+++ features/step_definition/steps-def.js
@@ -0,0 +1,30 @@
+const { Given, When, Then, AfterAll } = require('cucumber');
+const { Builder, By, Capabilities, Key } = require('selenium-webdriver');
+const { expect } = require('chai');
+
+require("chromedriver");
+
+// driver setup
+const capabilities = Capabilities.chrome();
+capabilities.set('chromeOptions', { "w3c": false });
+const driver = new Builder().withCapabilities(capabilities).build();
+
+Given('I am on the Google search page', async function () {
+    await driver.get('http://www.google.com');
+});
+
+When('I search for {string}', async function (searchTerm) {
+    const element = await driver.findElement(By.name('q'));
+    element.sendKeys(searchTerm, Key.RETURN);
+    element.submit();
+});
+
+Then('the page title should start with {string}', {timeout: 60 * 1000}, async function (searchTerm) {
+    const title = await driver.getTitle();
+    const isTitleStartWithCheese = title.toLowerCase().lastIndexOf(`${searchTerm}`, 0) === 0;
+    expect(isTitleStartWithCheese).to.equal(true);
+});
+
+AfterAll('end', async function(){
+    await driver.quit();
+});
