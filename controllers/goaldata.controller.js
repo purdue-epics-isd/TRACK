8138c658ee0f253d02b6c8cee98dd4550620b1ad
ShwethaUmashanker <shwthshanker@gmail.com>
2021-03-15 16:56:06 -0400

    Revert "Merge pull request #248 from purdue-epics-isd/qa-testing-andrew-style"
    This reverts commit 3277e9dc3100ce1adbc8773ce21f5adc780762b8, reversing
    changes made to df0a407fe378f1803e155e66fe6479bc88b531a4.

--- controllers/goaldata.controller.js
+++ controllers/goaldata.controller.js
@@ -49,10 +49,15 @@ exports.goaldata_create = async function (req, res) {
             });
         });
         
-
-        console.log("navigating to goal profile...");
-        setTimeout(() => { res.redirect("/student/" + req.params.studentid + "/goal/" + req.params.goalid); }, 1000);
+        res.redirect("/student/" + req.params.studentid);
         
+        // if(req.params.shared == "true") {
+        //     console.log("navigating to shared student profile...");
+        //     res.redirect("/sharedWithMe/" + req.params.studentid);
+        // } else {
+        //     console.log("navigating to personal student profile...");
+        //     res.redirect("/student/" + req.params.studentid);
+        // }
     } catch(err) {
         console.log(err);
         res.render('./error');
